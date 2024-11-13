5ba369bbbc3390ade08f63c861a07ee5849063f7
南秋SouthAki <xieleihan@gmail.com>
2024-11-13 11:53:55 +0800

    feat:新增我的页面

--- pages/manga/mangaClassification.vue
+++ pages/manga/mangaClassification.vue
@@ -7,7 +7,27 @@
 			</view>
 		</view>
 		<view class="right">
-			<div class="container"></div>
+			<view class="container">
+				<view class="title">{{titleName}}</view>
+				<view class="smallBox">
+					<view v-if="contentList.length === 0" class="noContent">
+						<img src="/static/icon/noContent.png" alt="" />
+						<span>暂无对应分类漫画</span>
+					</view>
+					<view v-if="contentList.length !== 0" class="content">
+						<view @click="getComicid" class="item" v-for="(item,index) in contentList" :key="index"
+							:data-comicid="item.comic_id">
+							<img :src="`${item.square_cover}`" alt="" />
+							<view class="title">
+								{{item.title}}
+							</view>
+							<view class="desc">
+								{{item.recommendation}}
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -18,6 +38,9 @@
 
 	const home_type = ref([]) // 首页分类类别
 	const home_index = ref(0) // 首页分类默认索引
+	const titleName = ref('热门') // 标题名
+	const tabId = ref(0) // tabId获取
+	const contentList = ref([]) // 分类漫画内容列表
 
 	onMounted(() => {
 		get('manga', 'GetClassPageAllTabs')
@@ -33,9 +56,47 @@
 	})
 
 	function selectItem(event : any) {
+		contentList.value = []
 		const target = event.currentTarget as HTMLElement;
 		const index = target.dataset.index; // 获取 data-index 值
 		home_index.value = Number(index)
+		titleName.value = home_type.value[Number(index)].name
+		tabId.value = home_type.value[Number(index)].tab_type
+		switch (tabId.value) {
+			case 6:
+				get('manga', 'GetClassPageSixComics?tabId=6')
+					.then((res) => {
+						console.log("这是6", res)
+						const str = JSON.stringify(res)
+						const obj = JSON.parse(str)
+						contentList.value = obj.data.roll_six_comics;
+					})
+					.catch((err) => {
+						console.log("获取分类里的信息错误", err)
+					})
+				break;
+			case 9:
+				get('manga', 'GetClassPageSixComics?tabId=9')
+					.then((res) => {
+						console.log("这是9", res)
+						const str = JSON.stringify(res)
+						const obj = JSON.parse(str)
+						contentList.value = obj.data.roll_six_comics;
+					})
+					.catch((err) => {
+						console.log("获取分类里的信息错误", err)
+					})
+				break;
+		}
+	}
+
+	function getComicid(event : any) {
+		const target = event.currentTarget as HTMLElement;
+		const comicid = target.dataset.comicid; // 获取 data-comicid 值
+		// console.log(comicid)
+		uni.navigateTo({
+			url: '/pages/manga/mangaInfo?comic_id=' + comicid
+		})
 	}
 </script>
 
@@ -75,6 +136,77 @@
 			.container {
 				width: 100%;
 				height: 100%;
+
+				.title {
+					width: 100%;
+					height: 50rpx;
+					font-weight: bold;
+				}
+
+				.smallBox {
+					width: 100%;
+					height: calc(100% - 50rpx);
+					border: 1px solid #ccc;
+					// border-radius: @radius;
+					box-sizing: border-box;
+					padding: 20rpx;
+
+					.content {
+						width: 100%;
+						height: 100%;
+						overflow-y: scroll;
+						display: flex;
+						flex-direction: row;
+						flex-wrap: wrap;
+						justify-content: space-between;
+
+						.item {
+							background-color: white;
+							width: 48%;
+							border: 1px solid #ccc;
+							border-radius: @radius;
+							box-sizing: border-box;
+							padding: 10rpx;
+							margin-bottom: 10rpx;
+
+							img {
+								width: 100%;
+								height: 200rpx;
+								border-radius: @radius;
+							}
+
+							.title {
+								font-weight: 500;
+								white-space: nowrap;
+								text-overflow: ellipsis;
+								overflow: hidden;
+							}
+
+							.desc {
+								display: -webkit-box;
+								-webkit-box-orient: vertical;
+								overflow: hidden;
+								text-overflow: ellipsis;
+								-webkit-line-clamp: 2;
+								color: #ccc;
+							}
+						}
+					}
+
+					.noContent {
+						width: 100%;
+						height: 100%;
+						display: flex;
+						justify-content: center;
+						align-items: center;
+						flex-direction: column;
+
+						img {
+							width: 90%;
+							aspect-ratio: 1/1;
+						}
+					}
+				}
 			}
 		}
 	}
--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,8 +1,124 @@
 <template>
+	<view class="user">
+		<view class="topBanner">
+			<img class="img" src="/static/images/bg7.jpg" alt="" />
+			<img class="avater" src="/static/images/avater.jpg" alt="" />
+			<view class="triangle"></view>
+			<img class="revise" src="/static/icon/pen.png" alt="" />
+		</view>
+		<view class="title">
+			<div class="topTitle">
+				<view class="username">这是用户名</view>
+				<view class="editBtn">
+					编辑资料
+				</view>
+			</div>
+			<view class="info">
+				你还没有填写简介信息
+			</view>
+		</view>
+
+		<view class="function">
+
+		</view>
+	</view>
 </template>
 
-<script>
+<script lang="ts" setup>
 </script>
 
-<style>
+<style lang="less" scoped>
+	@import url('../../static/public.less');
+
+	.user {
+		width: 100dvw;
+		height: @startHeight;
+		overflow-y: scroll;
+
+		.topBanner {
+			width: 100%;
+			height: 400rpx;
+			position: relative;
+
+			.img {
+				width: 100%;
+				height: 100%;
+			}
+
+			.avater {
+				width: 150rpx;
+				height: 150rpx;
+				border-radius: 50%;
+				border: 1px solid #ccc;
+				position: absolute;
+				bottom: -75rpx;
+				left: 10%;
+			}
+
+			.triangle {
+				position: absolute;
+				transform: rotate(-270deg) translateX(-50rpx) translateY(50rpx);
+				top: 0;
+				z-index: 992;
+				right: 0;
+				width: 0;
+				height: 0;
+				border-style: solid;
+				border-width: 100rpx 100rpx 100rpx 0;
+				/* 设置边框宽度 */
+				border-color: rgba(0, 0, 0, .7) transparent transparent transparent;
+				/* 设置边框颜色 */
+			}
+
+			.revise {
+				width: 50rpx;
+				height: 50rpx;
+				position: absolute;
+				top: 0;
+				right: 0;
+				z-index: 996;
+			}
+		}
+
+		.title {
+			width: 100%;
+			padding: 20rpx 30rpx;
+			box-sizing: border-box;
+			height: 100rpx;
+			margin-top: 75rpx;
+			display: flex;
+			flex-direction: column;
+
+			.topTitle {
+				width: 100%;
+				display: flex;
+				flex-direction: row;
+				justify-content: space-between;
+				align-items: center;
+				height: 50rpx;
+				padding-left: 40rpx;
+				box-sizing: border-box;
+
+				.username {
+					font-weight: bold;
+				}
+
+				.editBtn {
+
+					border-radius: @radius;
+					padding: 20rpx 30rpx;
+					background-color: @googleblue;
+					color: white;
+				}
+			}
+
+			.info {
+				color: #ccc;
+				font-size: 24rpx;
+				margin-top: 10rpx;
+				border-bottom: 1px solid #ccc;
+				padding-bottom: 10rpx;
+			}
+		}
+	}
 </style>
